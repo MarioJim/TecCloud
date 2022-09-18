e707a52f200950123ad1c3bdb76da046d33da4cf
MarioJim <mario.emilio.j@gmail.com>
2022-09-18 15:42:48 -0500

    Setup database and connection with API

---
+++ teccloud-api/src/db/models.ts
@@ -0,0 +1,237 @@
+import {
+  Model,
+  DataTypes,
+  CreationOptional,
+  InferAttributes,
+  InferCreationAttributes,
+} from 'sequelize';
+import { sequelize } from './index';
+
+export class Folder extends Model<
+  InferAttributes<Folder>,
+  InferCreationAttributes<Folder>
+> {
+  declare id: CreationOptional<number>;
+  declare parentId: number;
+  declare name: string;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+Folder.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    parentId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Folder,
+        key: 'id',
+      },
+    },
+    name: { type: DataTypes.STRING, allowNull: false },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  {
+    sequelize,
+    modelName: 'folders',
+    timestamps: true,
+  },
+);
+
+export class User extends Model<
+  InferAttributes<User>,
+  InferCreationAttributes<User>
+> {
+  declare id: CreationOptional<number>;
+  declare firstName: string;
+  declare lastName: string;
+  declare username: string;
+  declare password: string;
+  declare folderId: number;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    firstName: { type: DataTypes.STRING, allowNull: false },
+    lastName: { type: DataTypes.STRING, allowNull: false },
+    username: { type: DataTypes.STRING, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    folderId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Folder,
+        key: 'id',
+      },
+      allowNull: false,
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  {
+    sequelize,
+    modelName: 'users',
+    timestamps: true,
+  },
+);
+
+export class File extends Model<
+  InferAttributes<File>,
+  InferCreationAttributes<File>
+> {
+  declare id: CreationOptional<number>;
+  declare folderId: number;
+  declare name: string;
+  declare serverPath: string;
+  declare size: number;
+  declare accessByLink: 'private' | 'public';
+  declare lastViewed: Date;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+File.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    folderId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Folder,
+        key: 'id',
+      },
+    },
+    name: { type: DataTypes.STRING, allowNull: false },
+    serverPath: { type: DataTypes.STRING, allowNull: false },
+    size: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    accessByLink: {
+      type: DataTypes.ENUM,
+      values: ['private', 'public'],
+      defaultValue: 'private',
+      allowNull: false,
+    },
+    lastViewed: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  {
+    sequelize,
+    modelName: 'files',
+    timestamps: true,
+  },
+);
+
+export class FileAccess extends Model<
+  InferAttributes<FileAccess>,
+  InferCreationAttributes<FileAccess>
+> {
+  declare id: CreationOptional<number>;
+  declare fileId: number;
+  declare userId: number;
+  declare access: 'private' | 'public';
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+FileAccess.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    fileId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: File,
+        key: 'id',
+      },
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: 'id',
+      },
+      allowNull: false,
+    },
+    access: {
+      type: DataTypes.ENUM,
+      values: ['private', 'public'],
+      defaultValue: 'private',
+      allowNull: false,
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  {
+    sequelize,
+    modelName: 'file_access',
+    timestamps: true,
+  },
+);
+
+export class Page extends Model<
+  InferAttributes<Page>,
+  InferCreationAttributes<Page>
+> {
+  declare id: CreationOptional<number>;
+  declare fileId: number;
+  declare number: number;
+  declare thumbnailPath: string;
+  declare content: string;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+Page.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    fileId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: File,
+        key: 'id',
+      },
+      allowNull: false,
+    },
+    number: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    thumbnailPath: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  {
+    sequelize,
+    modelName: 'pages',
+    timestamps: true,
+  },
+);
