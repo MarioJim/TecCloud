89f0d369be38e7f95b194c01a35c1199f6b5e4d5
MarioJim <mario.emilio.j@gmail.com>
2022-09-27 14:41:30 -0500

    Upload dev.env to repo with settings for env vars

--- teccloud-api/src/main.ts
+++ teccloud-api/src/main.ts
@@ -1,30 +1,25 @@
 import * as dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: `${process.env.ENVIRONMENT}.env` });
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { sequelize } from './db';
 import UserRoutes from './controllers/User';
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(cookieParser());
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
-
-const port = process.env.PORT || 3001;
 
 // User routes
 app.use('/user', UserRoutes);
 
 (async () => {
   await sequelize.sync({ force: true });
+
+  const port = process.env.PORT || 3001;
   app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`);
   });
