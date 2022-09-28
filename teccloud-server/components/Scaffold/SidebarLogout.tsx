715bd4380c7636e2437bd1917f0494cfee6835d5
Angel-AG <miniprobecat@hotmail.com>
2022-09-28 14:00:54 -0500

    Implement logout

--- teccloud-server/components/Scaffold/SidebarLogout.tsx
+++ teccloud-server/components/Scaffold/SidebarLogout.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import Link from 'next/link';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import LogoutIcon from '@mui/icons-material/Logout';
 
-const logout = () => {
-  console.log('Clicking logout button!');
-};
-
 const SidebarLogout = () => {
+  const logout: (e: React.FormEvent) => void = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('http://localhost:3001/user/logout', {
+        method: 'post',
+        credentials: 'include',
+      });
+      sessionStorage.removeItem('user');
+      location.assign('/');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <ListItem disablePadding>
       <ListItemButton onClick={logout}>
