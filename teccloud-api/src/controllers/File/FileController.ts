3f1ac0e5be3c7a440c3cdf6d6b0aaed2b285fdd6
MarioJim <mario.emilio.j@gmail.com>
2022-11-18 10:07:01 -0600

    Fix

--- teccloud-api/src/controllers/File/FileController.ts
+++ teccloud-api/src/controllers/File/FileController.ts
@@ -226,26 +226,18 @@ class FileController {
 
       const folders = await Folder.findAll({ where: { parentId: folderId } });
 
-      const files = await user.getFiles({
+      let files = await user.getFiles({
         where: { folderId },
         include: [{ model: User }],
       });
-      if (await folder.isOwnedBy(user)) {
-        res.json({
-          files: files,
-          folders: folders,
-          parentId: folder.parentId,
-        });
-      } else {
+      if (!(await folder.isOwnedBy(user))) {
         const filePermissions = await Promise.all(
           files.map((file) => file.viewableBy(user)),
         );
-        res.json({
-          files: files.filter((_, i) => filePermissions[i]),
-          folders: folders,
-          parentId: folder.parentId,
-        });
+        files = files.filter((_, i) => filePermissions[i]);
       }
+
+      res.json({ files, folders, parentId: folder.parentId });
     };
   }
 
